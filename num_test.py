218131836ed9b7f360f25f8b51a8c7f17b8dc8e3
Rybka <nikitarybalkin1@gmail.com>
2024-02-01 09:04:28 +0600

    1

--- num_test.py
+++
@@ -1,6 +0,0 @@
-from libraries.word2numberi18n import w2n
-
-word_number = "десять"
-numeric_value = w2n.word_to_num(word_number)
-
-print(numeric_value)
---
+++ num_test_old.py
@@ -0,0 +1,6 @@
+from libraries.word2numberi18n import w2n
+
+def w2ns(str):
+    numeric_value = w2n.word_to_num(str)
+    return numeric_value
+
