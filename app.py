5f7d1d33ff4455968fa35e6246ad66332b7408fa
Rybka <nikitarybalkin1@gmail.com>
2024-02-01 19:25:48 +0600

    1.1

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from speech_old import process_audio_input
 from commands.modules.volume import volumeMax , volumeMute , volumeDown
+import keyboard
 
+
+print("Press any key to start audio processing or press 'Q' to quit.")
 while True:
+    
+    if keyboard.is_pressed('q'):
+        break
     result = process_audio_input()
     if (result != ""):
         if(result == "раз"):
-            print("test c")
+             
             volumeMax()
 
         if(result == "5"):
-            print("test c")
-            volumeMute()
             
+            volumeMute()
+
         if(result == "2"):
-            print("test c")
+           
             volumeDown()
     else:
         result = process_audio_input()
