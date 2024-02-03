44efc442efd84681ded7e96680cf871212e0103b
Rybka <nikitarybalkin1@gmail.com>
2024-02-03 17:17:30 +0600

    read update

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ while True:
     if result:
         print("Processed result:", result)
 
-        if (result == "громкость на 100" or result=="громкость на стол" ):
+        if (result == "громкость на 100" or result=="громкость на стол" ):#делае громкость на 100 , иногда слышит сто как стол
             volumeMax()
-        elif result == "выключи звук":
+        elif result == "выключи звук":#просто мут
             volumeMute()
-        elif result == "сделай потише":
-            volumeDown()
-        elif "яркость" in result:
+        elif result == "сделай потише": #звук убавляется на 2 единицы
+            volumeDown()   
+        elif "яркость" in result:#яркость на какая то цифра , изменяет яркость на столько
             brightness_value = ''.join(filter(str.isdigit, result))
             if brightness_value:
                 setBrightness(int(brightness_value))
